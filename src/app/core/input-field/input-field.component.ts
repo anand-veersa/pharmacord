6d7fc24dbffd50cd7858cb8f17855b8bb517839c
Ritik Kaushik <ritik.kaushik@veersaTech.com>
2023-02-14 16:54:46 +0530

    checking hook

--- src/app/core/input-field/input-field.component.ts
+++ src/app/core/input-field/input-field.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-
   selector: 'app-input-field',
   templateUrl: './input-field.component.html',
   styleUrls: ['./input-field.component.scss'],
@@ -9,11 +8,8 @@ import { Component } from '@angular/core';
 export class InputFieldComponent {
   private label: string;
   private type: string;
-  
   private placeholder: string;
   private validityErrorMsg: string;
   private reqErrorMsg: string;
   private formControlName: string;
-
-
 }
