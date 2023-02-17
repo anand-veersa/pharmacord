ad43f113427dfa8cddfac8ac151d0e78fe4fdffa
Ritik Kaushik <ritik.kaushik@veersaTech.com>
2023-02-17 15:52:58 +0530

    login form btn work

--- src/app/core/custom-input/custom-input.component.ts
+++ src/app/core/custom-input/custom-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { JsonFormControls } from 'src/app/models/json-form-data.model';
 
 @Component({
