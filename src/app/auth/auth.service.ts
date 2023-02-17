558e15d883ea502dbbc257c12ca0d990486c91be
anand-veersa <abhishek@veersalabs.com>
2023-02-17 15:21:06 +0530

    login-screen: login page responsive

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,16 +2,15 @@ import { Injectable } from '@angular/core';
 import { LocalStorageService } from '../shared/local-storage.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   constructor(private localStorage: LocalStorageService) {}
-  
+
   public isLoggedIn(): boolean {
     return this.localStorage.getItem('accessToken');
   }
-  
+
   public getUserRole(): string | null {
     return this.localStorage.getItem('userRole');
   }
--- src/app/shared/local-storage.service.ts
+++ src/app/shared/local-storage.service.ts
@@ -2,17 +2,14 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class LocalStorageService {
-
-  constructor() {}
-  
   public getItem(key: string): any {
     return !!localStorage.getItem(key);
   }
-  
+
   public setItem(key: string, value: any): void {
     localStorage.setItem(key, value);
   }
-  
+
   public clear(): void {
     localStorage.clear();
   }
