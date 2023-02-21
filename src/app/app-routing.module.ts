f47654e95b0f94176e39635f6c00ed8a288fde5d
Ritik Kaushik <ritik.kaushik@veersaTech.com>
2023-02-21 11:49:42 +0530

    forgot -username password screen work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotPasswordComponent } from './auth/pages/forgot-password/forgot-password.component';
+import { ForgotUsernameComponent } from './auth/pages/forgot-username/forgot-username.component';
 import { LoginComponent } from './auth/pages/login/login.component';
 
 const routes: Routes = [
@@ -11,6 +13,14 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'forgot-username',
+    component: ForgotUsernameComponent,
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPasswordComponent,
   },
 ];
 
---
+++ src/app/auth/pages/forgot-password/forgot-password.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormControl, FormGroup } from '@angular/forms';
+import { JsonFormData } from 'src/app/models/json-form-data.model';
+
+@Component({
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.scss'],
+})
+export class ForgotPasswordComponent implements OnInit {
+  public confirmUsernameEmailForm: FormGroup;
+  public formData: JsonFormData;
+  public inputFieldClass: string = 'secondary';
+  public formControlNameArray: Array<string> = ['username', 'email'];
+  public forgotPAsswordForm1: boolean = true;
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get('/assets/json/forgot-password-confirm-username-password-form.json')
+      .subscribe((formData: any) => {
+        this.formData = formData;
+      });
+
+    this.confirmUsernameEmailForm = new FormGroup({
+      username: new FormControl(null),
+      email: new FormControl(null),
+    });
+  }
+
+  onSubmit() {
+    console.log(
+      'forgot-username-form submit btn',
+      this.confirmUsernameEmailForm
+    );
+  }
+}
