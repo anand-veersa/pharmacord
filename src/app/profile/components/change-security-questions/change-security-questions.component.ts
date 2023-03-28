7aad382c4a771a735c5329c896486688530a5b9a
Ashish <ashish@veersatech,com>
2023-03-28 12:19:10 +0530

    sec question screen

--- src/app/profile/components/change-security-questions/change-security-questions.component.ts
+++ src/app/profile/components/change-security-questions/change-security-questions.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-change-security-questions',
   templateUrl: './change-security-questions.component.html',
   styleUrls: ['./change-security-questions.component.scss'],
 })
-export class ChangeSecurityQuestionsComponent {}
+export class ChangeSecurityQuestionsComponent {
+  constructor(private router: Router) {}
+  public navigateToHome(): void {
+    this.router.navigate(['/enrollment/dashboard']);
+  }
+}
